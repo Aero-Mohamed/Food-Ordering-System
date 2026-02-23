2b372d610735eff2aef8365011926fe60746e6c0
Mohamed Ahmed <aero.mohamed@icloud.com>
2026-02-23 06:59:33 +0200

    feat: init order service entities

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/order/service/domain/entity/OrderItem.java
@@ -0,0 +1,86 @@
+package com.food.order.service.domain.entity;
+
+import com.food.domain.entity.BaseEntity;
+import com.food.domain.valueobject.Money;
+import com.food.domain.valueobject.OrderId;
+import com.food.order.service.domain.valueobject.OrderItemId;
+
+public class OrderItem extends BaseEntity<OrderItemId> {
+    private OrderId orderId;
+    private final Product product;
+    private final int quantity;
+    private final Money price;
+    private final Money totalPrice;
+
+    private OrderItem(Builder builder) {
+        super.setId(builder.orderItemId);
+        product = builder.product;
+        quantity = builder.quantity;
+        price = builder.price;
+        totalPrice = builder.totalPrice;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public OrderId getOrderId() {
+        return orderId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public Money getTotalPrice() {
+        return totalPrice;
+    }
+
+    public static final class Builder {
+        private OrderItemId orderItemId;
+        private Product product;
+        private int quantity;
+        private Money price;
+        private Money totalPrice;
+
+        private Builder() {
+        }
+
+        public Builder orderItemId(OrderItemId val) {
+            orderItemId = val;
+            return this;
+        }
+
+        public Builder product(Product val) {
+            product = val;
+            return this;
+        }
+
+        public Builder quantity(int val) {
+            quantity = val;
+            return this;
+        }
+
+        public Builder price(Money val) {
+            price = val;
+            return this;
+        }
+
+        public Builder totalPrice(Money val) {
+            totalPrice = val;
+            return this;
+        }
+
+        public OrderItem build() {
+            return new OrderItem(this);
+        }
+    }
+}
