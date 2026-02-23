7a807c6f6eadc3ec85653c4eda26809bc874bece
Mohamed Ahmed <aero.mohamed@icloud.com>
2026-02-23 07:37:50 +0200

    feat: order state changing methods

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package com.food.order.service.domain.entity;
+
+import com.food.domain.entity.AggregateRoot;
+import com.food.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
--- order-service/order-domain/order-domain-core/src/main/java/com/food/order/service/domain/entity/Order.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/order/service/domain/entity/Order.java
@@ -34,6 +34,46 @@ public class Order extends AggregateRoot<OrderId> {
         validateItemsPrice();
     }
 
+    public void pay() {
+        if (orderStatus != OrderStatus.PENDING) {
+            throw new OrderDomainException("Order is not in pending state");
+        }
+        orderStatus = OrderStatus.PAID;
+    }
+
+    public void approve() {
+        if (orderStatus != OrderStatus.PAID) {
+            throw new OrderDomainException("Order is not in paid state");
+        }
+        orderStatus = OrderStatus.APPROVED;
+    }
+
+    public void initCancel(List<String> failureMessages) {
+        if(orderStatus != OrderStatus.PAID) {
+            throw new OrderDomainException("Order is not in paid state");
+        }
+        orderStatus = OrderStatus.CANCELLING;
+        updateFailureMessages(failureMessages);
+    }
+
+    public void cancel(List<String> failureMessages) {
+        if(!(orderStatus == OrderStatus.CANCELLING || orderStatus == OrderStatus.PENDING)) {
+            throw new OrderDomainException("Order is not in cancelling or pending state");
+        }
+        orderStatus = OrderStatus.CANCELLED;
+        updateFailureMessages(failureMessages);
+    }
+
+    private void updateFailureMessages(List<String> messages) {
+        if(this.failureMessages != null && messages != null){
+            this.failureMessages.addAll(messages.stream().filter(msg -> !msg.isEmpty()).toList());
+        }
+
+        if(this.failureMessages == null) {
+            this.failureMessages = messages;
+        }
+    }
+
     private void validateItemsPrice() {
         Money orderItemsTotal = items.stream().map(orderItem -> {
             validateItemPrice(orderItem);
