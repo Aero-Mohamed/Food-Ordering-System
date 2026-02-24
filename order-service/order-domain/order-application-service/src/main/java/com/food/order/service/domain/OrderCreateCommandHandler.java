fbd8ab8b7f554bc04861acdc75798a290bf59d24
Mohamed Ahmed <aero.mohamed@icloud.com>
2026-02-24 07:20:38 +0200

    feat: impl event publishing option 2 with spring event publisher

--- order-service/order-domain/order-application-service/src/main/java/com/food/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/order/service/domain/OrderCreateCommandHandler.java
@@ -28,19 +28,22 @@ public class OrderCreateCommandHandler {
     private final CustomerRepository customerRepository;
     private final RestaurantRepository restaurantRepository;
     private final OrderDataMapper orderDataMapper;
+    private final ApplicationDomainEventPublisher applicationDomainEventPublisher;
 
     public OrderCreateCommandHandler(
             OrderDomainService orderDomainService,
             OrderRepository orderRepository,
             CustomerRepository customerRepository,
             RestaurantRepository restaurantRepository,
-            OrderDataMapper orderDataMapper
+            OrderDataMapper orderDataMapper,
+            ApplicationDomainEventPublisher applicationDomainEventPublisher
     ) {
         this.orderDomainService = orderDomainService;
         this.orderRepository = orderRepository;
         this.customerRepository = customerRepository;
         this.restaurantRepository = restaurantRepository;
         this.orderDataMapper = orderDataMapper;
+        this.applicationDomainEventPublisher = applicationDomainEventPublisher;
     }
 
     @Transactional
@@ -51,6 +54,8 @@ public class OrderCreateCommandHandler {
         OrderCreatedEvent orderCreatedEvent = orderDomainService.validateAndInitiateOrder(order, restaurant);
         Order orderResult = saveOrder(order);
         log.info("Order with id: {} is created", orderResult.getId().getValue());
+
+        applicationDomainEventPublisher.publish(orderCreatedEvent);
 
         return orderDataMapper.orderToCreateOrderResponse(orderResult);
     }
